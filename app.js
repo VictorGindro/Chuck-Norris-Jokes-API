7f06daed48d6a5a8ae6fbead7e35132e171bab60
Renan <reebrando@gmail.com>
2018-12-14 12:04:23 -0200

    Inital commit with sample endpoints.

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const joke = require('./routes/joke.route'); 
+const app = express();
+
+// Set up mongoose connection
+let dev_db_url = 'mongodb://bringto:resource123@ds121248.mlab.com:21248/chuck-norris-jokes';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB);
+
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/joke', joke);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+})
